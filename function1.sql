0c78768330dfec7c79d89a16c10c9417affa3084
Taylor Basso <taylorb@tazworks.com>
2018-03-17 12:39:56 -0600

    Function1.sql

---
+++ function1.sql
@@ -0,0 +1,28 @@
+/*Create a function (or functions) that will convert TB to GB and GB to TB. 
+Looking at the database, you can see that hard drive space is stored in GB 
+but the developer expects to be able to display TB (and doesn't want to do 
+the conversion themselves). They'll also be passing back TB when they manipulate 
+computer inventory and you'll need to convert to GB to store.*/
+DROP FUNCTION IF EXISTS Group3_GBtoTBConverter
+CREATE FUNCTION Group3_GBtoTBConverter
+(
+	@num1 DECIMAL(18,10)
+)
+RETURNS DECIMAL(18,10)
+AS BEGIN
+	RETURN 1.0 * (@num1 / 1024)
+END
+
+--SELECT dbo.Group3_GBtoTBConverter(131.12);
+
+DROP FUNCTION IF EXISTS Group3_TBtoGBConverter
+CREATE FUNCTION Group3_TBtoGBConverter
+(
+	@num1 DECIMAL(18,10)
+)
+RETURNS DECIMAL(18,10)
+AS BEGIN
+	RETURN 1.0 * (@num1 * 1024)
+END
+
+SELECT dbo.Group3_TBtoGBConverter(2.11);
