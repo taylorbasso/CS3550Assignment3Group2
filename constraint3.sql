769d788220e2ffae3c8cef922a7957b63cb29a1b
Taylor Basso <taylorb@tazworks.com>
2018-03-17 15:01:02 -0600

    Constraint3 init

---
+++ constraint3.sql
@@ -0,0 +1,8 @@
+/* Your compliance officer is adamant that certain safegaurds are installed in the database. 
+Consider the following:
+
+Worried about piracy and bandwidth consumption, they have also institued a hard cap of hard 
+drive space on any givenmachine to 5TB. */
+
+ALTER TABLE Computers
+ADD CONSTRAINT ...
