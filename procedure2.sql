343eb6d09bad653a891c4b9822c7a8d20792ea2b
Taylor Basso <taylorb@tazworks.com>
2018-03-17 13:14:58 -0600

    Progress on proc 2

--- procedure2.sql
+++ procedure2.sql
@@ -4,5 +4,20 @@ web developer to pass you keys (not the string values). You however cannot expec
 always pass you valid information... Make sure you take this into account as you're designing/coding the following
 stored procedures.
 
-Adding new employees. Return the key of the newly created employee, or an error message/code if something doesn't work.*/
+Adding new employees. 
+Return the key of the newly created employee, or an error message/code if something doesn't work.*/
 
+CREATE OR ALTER PROCEDURE Group3_InsertEmployee
+	@FirstName varchar(50),
+	@LastName varchar(50)
+AS 
+	INSERT Employees 
+	(
+		FirstName, 
+		LastName
+	) 
+	VALUES 
+	(
+		@FirstName, 
+		@FirstName
+	)
