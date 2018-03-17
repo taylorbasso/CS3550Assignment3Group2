d883fb64622ca644c505395f3f6bfee2d22f2346
Taylor Basso <taylorb@tazworks.com>
2018-03-17 14:15:06 -0600

    procedure9 init

---
+++ procedure9.sql
@@ -0,0 +1,9 @@
+/* Create stored procedures that complete the below tasks. You can combine as
+ you feel is appropriate. As a general rule of thumb, you can expect the web developer
+  to pass you keys (not the string values). You however cannot expect that they'll
+  always pass you valid information... Make sure you take this into account as you're
+   designing/coding the following stored procedures.
+
+Change the status of a computer (lost computers, in for repairs, etc.) */
+
+CREATE OR ALTER PROCEDURE Group3_UpdateComputerStatus
