8f707a51bc9bb1286b073b8340ac2c2ebe5b3070
Taylor Basso <taylorb@tazworks.com>
2018-03-17 12:42:34 -0600

    procedure1.sql

---
+++ procedure2.sql
@@ -0,0 +1,8 @@
+/*Create stored procedures that complete the below tasks. 
+You can combine as you feel is appropriate. As a general rule of thumb, you can expect the 
+web developer to pass you keys (not the string values). You however cannot expect that they'll
+always pass you valid information... Make sure you take this into account as you're designing/coding the following
+stored procedures.
+
+Adding new employees. Return the key of the newly created employee, or an error message/code if something doesn't work.*/
+
