f05d661cbb7c3cfda72a1c963b59f8b0eff43fc5
Alex Knight <ak1knight@gmail.com>
2018-03-19 10:46:10 -0600

    View 2
    Finished View 2.
    
    I also added create and drop tables files because the view needed an additional field in the Computers table to track how long a computer has had it's status. I also added a couple of rows to the inserts to test the view.
    
    **Be sure to always have the latest create_tables file to avoid merge conflicts**

---
+++ drop_tables.sql
@@ -0,0 +1,23 @@
+DROP TABLE EmployeeComputers
+;
+
+DROP TABLE Computers
+;
+
+DROP TABLE Employees
+;
+
+DROP TABLE Brands
+;
+
+DROP TABLE ComputerTypes
+;
+
+DROP TABLE ComputerStatuses
+;
+
+DROP TABLE CPUTypes
+;
+
+DROP TABLE Departments
+;
