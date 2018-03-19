a16454cb0e665cf396c46ffb515d3e9a7087f438
Eric Smith <ejs138@gmail.com>
2018-03-19 17:47:55 -0600

    #6 - add primary key

--- Table1_ComputerHistory.sql
+++ Table1_ComputerHistory.sql
@@ -1,5 +1,6 @@
 CREATE TABLE Group2_ComputerHistory
 (
+	ComputerHistoryKey int IDENTITY(1,1) PRIMARY KEY NOT NULL 
 	ComputerKey int NOT NULL,
 	OldComputerStatusKey int,
 	NewComputerStatusKey int NOT NULL,
