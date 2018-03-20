c44a650d88e4e11337417ef988de978928a6a0a2
Eric Smith <ejs138@gmail.com>
2018-03-19 18:59:35 -0600

    #6 - Add possibility of another table
    It's commented out, so we have it later if we need it (for the views). I'll figure out if we need it when I start on the views.

--- Table1_ComputerHistory.sql
+++ Table1_ComputerHistory.sql
@@ -1,29 +1,61 @@
 /* 
-Decided to rely on the EmployeeComputers table to link up the Computers to the status history
+Just deal with the status?
 
 */
 
-CREATE TABLE Group2_ComputerHistory
+CREATE TABLE Group2_ComputerStatusHistory
 (
-	ComputerHistoryKey int IDENTITY(1,1) PRIMARY KEY NOT NULL 
-	EmployeeComputerKey int NOT NULL,
-	OldComputerStatusKey int,
+	ComputerStatusHistoryKey int IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	ComputerKey int NOT NULL,
+	OldComputerStatusKey int NULL,
 	NewComputerStatusKey int NOT NULL,
 	StatusDateTime datetime NOT NULL DEFAULT GETDATE()
 )
 
-ALTER TABLE Group2_ComputerHistory
-	ADD CONSTRAINT FK_Group2_ComputerHistoryEmployeeComputer
-	FOREIGN KEY (EmployeeComputerKey)
-	REFERENCES EmployeeComputers (EmployeeComputerKey)
+ALTER TABLE Group2_ComputerStatusHistory
+	ADD CONSTRAINT FK_Group2_ComputerStatusHistoryComputer
+	FOREIGN KEY (ComputerKey)
+	REFERENCES Computers (ComputerKey)
 	
-ALTER TABLE Group2_ComputerHistory
-	ADD CONSTRAINT FK_Group2_ComputerHistoryOldStatus
+ALTER TABLE Group2_ComputerStatusHistory
+	ADD CONSTRAINT FK_Group2_ComputerStatusHistoryOldStatus
 	FOREIGN KEY (OldComputerStatusKey)
 	REFERENCES ComputerStatuses(ComputerStatusKey)
 	
-ALTER TABLE Group2_ComputerHistory
-	ADD CONSTRAINT FK_Group2_ComputerHistoryNewStatus
+ALTER TABLE Group2_ComputerStatusHistory
+	ADD CONSTRAINT FK_Group2_ComputerStatusHistoryNewStatus
 	FOREIGN KEY (NewComputerStatusKey)
 	REFERENCES ComputerStatuses(ComputerStatusKey)
 	
+/*
+
+Is this needed?
+
+The EmployeeComputer table should be enough for the history tracking, since each entry should be a single 
+assignment
+
+CREATE TABLE Group2_EmployeeComputerHistory
+(
+	EmployeeComputerHistoryKey int IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	ComputerKey int NOT NULL,
+	OldEmployeeKey int NULL,
+	NewEmployeeKey int NOT NULL,
+	StatusDateTime datetime NOT NULL DEFAULT GETDATE()
+)
+
+ALTER TABLE Group2_EmployeeComputerHistory
+	ADD CONSTRAINT FK_Group2_EmployeeComputerHistoryComputer
+	FOREIGN KEY (ComputerKey)
+	REFERENCES EmployeeComputers (ComputerKey)
+	
+ALTER TABLE Group2_EmployeeComputerHistory
+	ADD CONSTRAINT FK_Group2_EmployeeComputerHistoryOldEmployee
+	FOREIGN KEY (OldEmployeeKey)
+	REFERENCES EmployeeComputers (EmployeeKey)
+	
+ALTER TABLE Group2_EmployeeComputerHistory
+	ADD CONSTRAINT FK_Group2_EmployeeComputerHistoryNewEmployee
+	FOREIGN KEY (NewEmployeeKey)
+	REFERENCES EmployeeComputers (EmployeeKey)
+	
+*/
