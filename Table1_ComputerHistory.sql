4e3953a68b5d4108fe238845234762c6daba5509
Eric Smith <ejs138@gmail.com>
2018-03-19 18:04:26 -0600

    #6 - changed layout
    Use the EmployeeComputers as the link to the actual assignment, instead of trying to contain the assignment in here...

--- Table1_ComputerHistory.sql
+++ Table1_ComputerHistory.sql
@@ -1,8 +1,29 @@
+/* 
+Decided to rely on the EmployeeComputers table to link up the Computers to the status history
+
+*/
+
 CREATE TABLE Group2_ComputerHistory
 (
 	ComputerHistoryKey int IDENTITY(1,1) PRIMARY KEY NOT NULL 
-	ComputerKey int NOT NULL,
+	EmployeeComputerKey int NOT NULL,
 	OldComputerStatusKey int,
 	NewComputerStatusKey int NOT NULL,
 	StatusDateTime datetime NOT NULL DEFAULT GETDATE()
 )
+
+ALTER TABLE Group2_ComputerHistory
+	ADD CONSTRAINT FK_Group2_ComputerHistoryEmployeeComputer
+	FOREIGN KEY (EmployeeComputerKey)
+	REFERENCES EmployeeComputers (EmployeeComputerKey)
+	
+ALTER TABLE Group2_ComputerHistory
+	ADD CONSTRAINT FK_Group2_ComputerHistoryOldStatus
+	FOREIGN KEY (OldComputerStatusKey)
+	REFERENCES ComputerStatuses(ComputerStatusKey)
+	
+ALTER TABLE Group2_ComputerHistory
+	ADD CONSTRAINT FK_Group2_ComputerHistoryNewStatus
+	FOREIGN KEY (NewComputerStatusKey)
+	REFERENCES ComputerStatuses(ComputerStatusKey)
+	
