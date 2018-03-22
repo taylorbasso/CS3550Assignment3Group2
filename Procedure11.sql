78689b14a382269e63219adcd7c62e23b464346c
AaronNybo <aaron.nybo@gmail.com>
2018-03-22 12:57:49 -0600

    Update Procedure11.sql

--- Procedure11.sql
+++ Procedure11.sql
@@ -1,11 +1,20 @@
 --Retire a computer. Make sure the computer is fully depreciated, or do not let it be retired.
 
 CREATE PROCEDURE Group3_RetireComputer
-	@computerKey int
+	@computerKey int,
+	@computerOriginalCost money,
+	@computerTypeKey int,
+	@purchaseDate date,
+	@depreciation int
 
 AS
+	SET
+		@depreciation = @computerOriginalCost - dbo.Group2_CalculateDepreciation (@computerTypeKey, @computerOriginalCost, @purchaseDate)
+	
 	UPDATE dbo.Computers
 	SET
 		ComputerStatusKey = 5
 	WHERE
 		ComputerKey = @computerKey
+		AND
+		@depreciation <= 0
