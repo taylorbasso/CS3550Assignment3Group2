439c81d10c2cb52508011e0cf12f5804ff9e11ff
AaronNybo <aaron.nybo@gmail.com>
2018-03-22 12:59:21 -0600

    Update View3.sql

--- View3.sql
+++ View3.sql
@@ -1,15 +1,17 @@
 /*Create a view that returns an employee list - 
 should include their full name, email address,
 their department, the number of computers they have, 
-and who their supervisor is. Onlyreturn active employees.*/
+and who their supervisor is. Only return active employees.*/
 
 CREATE VIEW Group3_Employees
 AS
 SELECT
-	FirstName + ' ' + LastName [FullName],
-	Email,
+	E.FirstName + ' ' + E.LastName [FullName],
+	E.Email,
 	Department,
-	COUNT(EmployeeComputerKey) [Computers]
+	COUNT(EmployeeComputerKey) [Computers],
+	E2.FirstName + ' ' + E2.LastName [SupervisorFullName]
+
 
 FROM
 	dbo.Employees E
@@ -17,8 +19,16 @@ FROM
 		ON E.EmployeeKey = EC.EmployeeKey
 	LEFT JOIN dbo.Departments D
 		ON E.DepartmentKey = D.DepartmentKey
+	INNER JOIN Employees E2
+		ON E2.EmployeeKey = E.SupervisorEmployeeKey
+
+WHERE
+	E.Terminated IS NULL
+
 GROUP BY
-	FirstName,
-	LastName,
-	Email,
-	Department
+	E.FirstName,
+	E.LastName,
+	E.Email,
+	Department,
+	E2.FirstName,
+	E2.LastName
