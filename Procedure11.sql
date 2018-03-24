2cb72c0bbdfaaf204176e23a984ea289f61a8028
Alex Knight <ak1knight@gmail.com>
2018-03-24 10:46:58 -0600

    Queries 5
    Finished Queries 5 and modified procedure 11 to get the data it needs instead of requesting from the user.

--- Procedure11.sql
+++ Procedure11.sql
@@ -1,13 +1,21 @@
 --Retire a computer. Make sure the computer is fully depreciated, or do not let it be retired.
 
-CREATE PROCEDURE Group3_RetireComputer
-	@computerKey int,
-	@computerOriginalCost money,
-	@computerTypeKey int,
-	@purchaseDate date,
-	@depreciation int
+CREATE OR ALTER PROCEDURE Group3_RetireComputer
+	@computerKey int
 
 AS
+	DECLARE @depreciation money,
+			@computerOriginalCost money,
+			@computerTypeKey int,
+			@purchaseDate date
+
+	SELECT
+		@computerOriginalCost = PurchaseCost,
+		@computerTypeKey = ComputerTypeKey,
+		@purchaseDate = PurchaseDate
+	FROM Computers
+	WHERE ComputerKey = @computerKey
+
 	SET
 		@depreciation = @computerOriginalCost - dbo.Group2_CalculateDepreciation (@computerTypeKey, @computerOriginalCost, @purchaseDate)
 	
---
+++ Queries 5.sql
@@ -0,0 +1,13 @@
+/*
+Try to retire the second machine you assigned to the CEO (he's picky...)
+*/
+
+EXEC Group3_RetireComputer 6
+
+/*
+Query that leverages your view for depreciation values of lost equipment
+*/
+
+SELECT
+	*
+FROM Group2_LostStolenComputers
