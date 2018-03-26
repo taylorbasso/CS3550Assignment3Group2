7458917e6d5213d3cc23b68a1c759d7e2cd8ec9e
Alex Knight <ak1knight@gmail.com>
2018-03-26 16:02:04 -0600

    Constraint 1 fix
    Should only raise an error if the computer type to be inserted would put the employee above the limit for that type.

--- Constraint1_AssignComputers.sql
+++ Constraint1_AssignComputers.sql
@@ -5,7 +5,7 @@ ON dbo.EmployeeComputers
 INSTEAD OF INSERT
 AS
 	BEGIN
-		DECLARE @desktopcount INT, @laptopcount INT
+		DECLARE @desktopcount INT, @laptopcount INT, @insertedCompType INT
 		SELECT
 			@desktopcount = COUNT(CASE c.ComputerTypeKey WHEN 1 THEN 1 ELSE NULL END),
 			@laptopcount = COUNT(CASE c.ComputerTypeKey WHEN 2 THEN 1 ELSE NULL END)
@@ -15,9 +15,16 @@ AS
 			ec.ComputerKey = c.ComputerKey
 		WHERE
 			EmployeeKey = (SELECT EmployeeKey FROM inserted) AND
-			c.ComputerStatusKey < 3
+			c.ComputerStatusKey < 3 AND
+			ec.Returned IS NULL
 
-		IF (@desktopcount >= 2 OR @laptopcount >= 1)
+		SELECT
+			@insertedCompType = c.ComputerTypeKey
+		FROM inserted i
+		LEFT JOIN Computers c ON 
+			i.ComputerKey = c.ComputerKey
+
+		IF ((@insertedCompType = 1 AND @desktopcount >= 2) OR (@insertedCompType = 2 AND @laptopcount >= 1))
 			BEGIN
 				RAISERROR('No individual can have more than two working desktops or one working laptop assigned to them at any given time.', 1, 1);
 			END
