19270bf1a8541cd2ee4bacf80bdbf5dc97aee0f2
Nicholas Lambert <nick@thedesigniac.com>
2018-03-26 20:32:00 -0600

    Constraint4 part dos

--- Constraint4.sql
+++ Constraint4.sql
@@ -3,18 +3,25 @@ Your compliance officer is adamant that certain safegaurds are installed in the 
 Consider the following:
 
 Do not let anyone delete a computer record. Can't happen! We have to prove that no
-equipment is being purchasedand removed from our systems to sell privately. Instead
-of a delete, the computer should be retired, ormade available (depending on its depreciation status).
+equipment is being purchased and removed from our systems to sell privately. Instead
+of a delete, the computer should be retired, or made available
 */
 
 CREATE TRIGGER Group3_DontSell
-ON dbo.ComputerStatuses
+ON dbo.Computers
 INSTEAD OF DELETE
 AS
 	BEGIN
-		IF ComputerStatus < 5
-		THEN
-				 UPDATE ComputerStatuses SET ComputerStatus = 2
+		SELECT
+			ComputerStatusKey
+		FROM
+			Computers
+		IF ComputerStatusKey < 5
+			BEGIN
+				UPDATE ComputerStatuses SET ComputerStatus = 2
+			END
 		ELSE
-			UPDATE ComputerStatuses SET ComputerStatus = 5
+			BEGIN
+				UPDATE ComputerStatuses SET ComputerStatus = 5
+			END
 	END
